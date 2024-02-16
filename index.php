294e119e930c9b42f0fa1fd189fb14448ad586ee
zacky saputra <160191856+kiboy16@users.noreply.github.com>
2024-02-16 13:52:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman landing</title>
+</head>
+<body>
+    <h1>Halaman landing</h1>
+    <ul>
+        <li><a href="login.php">login</a></li>
+        <li><a href="register.php">register</a></li>
+    </ul>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman register</title>
+</head>
+<body>
+    <h1>halaman register</h1>
+    <form action="proses-register.php" method="post">
+        <table>
+            <tr>
+                <td>username</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td>email</td>
+                <td><input type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>nama lengkap</td>
+                <td><input type="text" name="namalengkap"></td>
+            </tr>
+            <tr>
+                <td>alamat</td>
+                <td><input type="text" name="alamat"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="register"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
